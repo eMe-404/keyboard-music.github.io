5c3ea627ac4e35177d3b1fff071e2a9113660008
eMe <838702939@qq.com>
2017-10-13 22:03:07 +0800

    use preload audio

--- Demo1_JS.js
+++ Demo1_JS.js
@@ -1,10 +1,10 @@
 const bar = document.querySelectorAll(".bar");
 bar.forEach(b => b.style.animation = "unset"); //first enter the page ,there should no be any animation
 
-const allAudio = document.querySelectorAll("audio"); //preload the music
+/*const allAudio = document.querySelectorAll("audio"); //preload the music but it can`t help github still can`t play the sound
 allAudio.forEach(function (audio) {
     audio.setAttribute("preload","auto");
-});
+});*/
 
 function playSound(e) {
     e.preventDefault();      //can prevent the space pressed fox problem
